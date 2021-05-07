da1ee680376995f1a81dd10188a7a71a30a86702
Adeola Omonira <adomonir@microsoft.com>
2021-05-07 10:19:07 -0400

    Updated Manage Controller AB#10079

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Controllers/ManageController.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Controllers/ManageController.cs
@@ -10,7 +10,7 @@ using PartsUnlimited.Models;
 namespace PartsUnlimited.Controllers
 {
     [Authorize]
-    //Adeola
+    //Adeola1
     public class ManageController : Controller
     {
         private SignInManager<ApplicationUser, string> SignInManager
